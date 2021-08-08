ca35783cc1bb8a08b8f339720430e3efde29c171
lordirah <lordirah@gmail.com>
2021-08-08 18:53:15 +0530

    Adding list vs dict compare

---
+++ my_sql_connect.py
@@ -0,0 +1,23 @@
+import mysql.connector
+import csv
+
+conn = mysql.connector.connect(host = 'localhost', database = 'employees', user = 'root' , password = 'root')
+cur = conn.cursor()
+cur.execute("show tables;")
+out = cur.fetchall()
+fs = open('/home/lordirah/Documents/table_list.csv', 'w')
+
+c = csv.writer(fs)
+for x in out:
+    c.writerow(x)
+fs.close()
+
+fs = open('/home/lordirah/Documents/table_list.csv', 'r')
+for table in fs:
+    cur.execute(f"select * from {table};")
+    out = cur.fetchall()
+    fs = open(f'/home/lordirah/Documents/{table}.csv', 'w')
+    c = csv.writer(fs)
+    for x in out:
+        c.writerow(x)
+    fs.close()
